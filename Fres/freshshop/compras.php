ddcb0dc907cef3c3f71d851fb66ad91b5885eaae
Marvin <marvin1ronal@gmail.com>
2019-12-26 19:04:44 -0600

    Compras inicio

---
+++ Fres/freshshop/compras.php
@@ -0,0 +1,29 @@
+<?php
+include "arriba.php";
+ ?>
+ <div class="categories-shop">
+        <div class="container">
+            <div class="row">
+                <div class="container">
+                    <h2>Articulos para la compra</h2>
+                    <div class="panel-group">
+
+                        <div class="panel panel-success">
+                            <div class="panel-heading">Agregue productos al carrito</div>
+                            <div class="panel-body">
+
+                                
+
+
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+ <?
+ include "abajo.php";
+ ?>
