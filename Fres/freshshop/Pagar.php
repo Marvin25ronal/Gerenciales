e1ec7b00da67c4fe5f26764cf9f3bca166166201
Marvin <marvin1ronal@gmail.com>
2019-12-26 23:39:47 -0600

    Inicio pago

---
+++ Fres/freshshop/Pagar.php
@@ -0,0 +1,25 @@
+<?php
+include "arriba.php";
+include "Funciones.php";
+?>
+<div class="categories-shop">
+    <div class="container">
+        <div class="row">
+            <div class="container">
+                <h2>Comprobante</h2>
+                <div class="panel-group">
+                    <div class="panel panel-info">
+                        <div class="panel-heading">Este es el comprobante de compra</div>
+                        <div class="panel-body">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include "abajo.php";
+?>
